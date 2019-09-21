d2a0f837c697a9c2f2818521d41e326353e6095e
PrijalBista <pri1.zzal@gmail.com>
2019-09-21 23:35:47 +0545

    Improved code Editor Integrated CodeMirror Editor with C++ enabled syntax highlighting

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,26 @@
 const socket = io.connect("http://192.168.1.162:3000");
 
-let editor = document.getElementById('editor');
-editor.contentEditable = true;
-editor.focus();
+let editor = CodeMirror(document.body, {
+  value: "#include <stdio.h>\n \nint main(){\n \n}",
+  //mode:  "javascript",
+  mode: "text/x-c++src",
+  lineNumbers: true,
+  tabSize:2,
+});
 
 
-handleContentChange = (e)=> {
-    socket.emit('client-edits-content', { text:e.target.innerHTML });
+handleContentChange = (changeObj)=> {
+    socket.emit('client-edits-content', { text:editor.getValue() });
 }
+// Whenever User use editor to make changes to the content propagate this change to all other clients 
+editor.on('keyup', handleContentChange );
 
-editor.addEventListener( 'input', handleContentChange);
-
-// When opened for the first time update editor with existing data
 updateContent= data => {
-    editor.innerHTML = data.text;
+	if(data.text=='') return;
+    editor.setValue(data.text);
 }
+// When opened for the first time update editor with existing data
 socket.on('newUser', updateContent);
 
+// When any other client makes changes to the content update the editor with updatedContent
 socket.on('other-client-edited-content', updateContent);
