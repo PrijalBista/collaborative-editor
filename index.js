2386bb4b42a58785fd94c31471a272f8ac180c2b
PrijalBista <pri1.zzal@gmail.com>
2019-09-30 00:09:18 +0545

    Added an endPoint for compiling code runCode/:lang endpoint Added c/c++ program compiled and run on server and output is sent to client as obj {stdout, stderr}

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 
 const express = require('express');
 const socket = require('socket.io');
+const { exec } = require('child_process');
 
 const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+app.get('/runCode/:lang' , (req, res) => {
+    if(req.params.lang=='cpp') {
+        let cmd = `
+        echo '${text.text}' | g++ -x c++ - && ./a.out`;
+        exec(cmd, (err, stdout, stderr) => {
+            //if(err) return;
+            res.json({stdout, stderr});
+        });
+    }
 });
 const server = app.listen( process.env.PORT || 3000, () => console.log('server up and running') );
 
